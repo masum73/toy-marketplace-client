d9f250662d4855383ef0c3a34c659007e3a43d63
Masum Rahman <masumrahman73@gmail.com>
2023-06-12 13:47:46 +0600

    my toys and toy details updated

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../provider/AuthProvider';
-import { Button, Card, CardBody, CardFooter, CardHeader, Typography } from '@material-tailwind/react';
-import { Link, useParams } from 'react-router-dom';
+import {  Card, Typography } from '@material-tailwind/react';
+import { Link } from 'react-router-dom';
 
 const MyToys = () => {
     const { user } = useContext(AuthContext);
     const [toys, setToys] = useState([]);
-
+    const TABLE_HEAD = ["Name", "Price", "Quantity","Description"," ", " "];
 
     const url = `http://localhost:5000/mytoys?email=${user?.email}`;
 
@@ -21,41 +21,88 @@ const MyToys = () => {
             })
     }, [])
 
+    const handleDelete = id => {
+        const proceed = confirm('Are you sure you want to delete?');
+        if(proceed){
+            fetch(`http://localhost:5000/mytoys/${id}`, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.deletedCount > 0){
+                    alert('Delete successful');
+                    const remaining = toys.filter(toy => toy._id !== id);
+                    setToys(remaining);
+                }
+            })
+        }
+    }
+
     return (
         <div>
             <h2 className='text-center text-4xl my-16'>My Toys</h2>
-            {
-                toys.map(toy => <Card key={toy._id} className="w-96">
-                    <CardHeader shadow={false} floated={false} className="h-96">
-                        <img
-                            src={toy.picture}
-                            className="w-full h-full object-cover"
-                        />
-                    </CardHeader>
-                    <CardBody>
-                        <div className="flex items-center justify-between mb-2">
-                            <Typography color="blue-gray" className="font-medium">
-                                {toy.name}
-                            </Typography>
-                            <Typography color="blue-gray" className="font-medium">
-                                ${toy.price}
-                            </Typography>
-                        </div>
-                        <Typography variant="small" color="gray" className="font-normal opacity-75">
-                            {toy.description}
-                        </Typography>
-                    </CardBody>
-                    <CardFooter className="pt-0">
-                        <Link to={`/mytoys/${toy._id}`}><Button
-                            ripple={false}
-                            fullWidth={true}
-                            className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
-                        >
-                            View Details
-                        </Button></Link>
-                    </CardFooter>
-                </Card>)
-            }
+            <Card className="overflow-scroll h-full w-full">
+                <table className="w-full min-w-max table-auto text-left">
+                    <thead>
+                        <tr>
+                            {TABLE_HEAD.map((head,index) => (
+                                <th key={index} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="font-normal leading-none opacity-70"
+                                    >
+                                        {head}
+                                    </Typography>
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {toys.map(({ _id ,name, price, quantity,description }, index) => {
+                            const isLast = index === toys.length - 1;
+                            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+
+                            return (
+                                <tr key={_id}>
+                                    <td className={classes}>
+                                        <Typography variant="small" color="blue-gray" className="font-normal">
+                                            {name}
+                                        </Typography>
+                                    </td>
+                                    <td className={classes}>
+                                        <Typography variant="small" color="blue-gray" className="font-normal">
+                                            {price}
+                                        </Typography>
+                                    </td>
+                                    <td className={classes}>
+                                        <Typography variant="small" color="blue-gray" className="font-normal">
+                                            {quantity}
+                                        </Typography>
+                                    </td>
+                                    <td className={classes}>
+                                        <Typography variant="small" color="blue-gray" className="font-normal">
+                                            {description}
+                                        </Typography>
+                                    </td>
+                                    <td className={classes}>
+                                        <Link to={`/mytoys/${_id}`}><Typography  variant="small" color="blue" className="font-medium">
+                                            Edit
+                                        </Typography></Link>
+                                        
+                                    </td>
+                                    <td className={classes}>
+                                        <Typography onClick={()=>handleDelete(_id)} variant="small" color="blue" className="font-medium cursor-pointer">
+                                            Delete
+                                        </Typography>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </Card>
         </div>
     );
 };
--- src/pages/ToyDetails/ToyDetails.jsx
+++ src/pages/ToyDetails/ToyDetails.jsx
@@ -6,29 +6,21 @@ import { useParams } from 'react-router-dom';
 const ToyDetails = () => {
     const { id } = useParams();
     const [toy, setToy] = useState(null);
-    const [isEditable, setIsEditable] = useState(false);
 
     const handleUpdateToy = (event) => {
         event.preventDefault();
 
         const form = event.target;
 
-        const photo = form.photo.value;
-        const name = form.name.value;
-        const subCategory = form.subCategory.value;
         const price = form.price.value;
-        const rating = form.rating.value;
         const quantity = form.quantity.value;
         const description = form.description.value;
 
-        
+
 
         const updateToy = {
-            name,
-            picture: photo,
+
             price,
-            rating,
-            subCategory,
             quantity,
             description
 
@@ -45,12 +37,11 @@ const ToyDetails = () => {
             .then(res => res.json())
             .then(data => {
                 console.log(data);
-                if(data.acknowledged){
-                    setToy({...updateToy})
-                    setIsEditable(false)
+                if (data.acknowledged) {
+                    setToy({ ...updateToy })
                     alert('toy updated successfully');
                 }
-                
+
 
             })
     }
@@ -74,23 +65,14 @@ const ToyDetails = () => {
 
                 <form onSubmit={handleUpdateToy} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                     <div className="mb-4 flex flex-col gap-6">
-                        {isEditable ?
-                            <Input defaultValue={toy?.picture} size="lg" type='url' name='photo' label="Photo URL" />
-                            : <img className='w-fit' src={toy?.picture}></img>}
-
-                        <Input disabled={!isEditable} defaultValue={toy?.name} size="lg" type='text' name='name' label="Name" />
-                        <Input disabled={!isEditable} defaultValue={toy?.subCategory} size="lg" type='text' name='subCategory' label="Sub-Category" />
-                        <Input disabled={!isEditable} defaultValue={toy?.price} size="lg" type='number' name='price' label="Price" />
-                        <Input disabled={!isEditable} defaultValue={toy?.rating} size="lg" type='number' name='rating' label="Rating" />
-                        <Input disabled={!isEditable} defaultValue={toy?.quantity} size="lg" type='number' name='quantity' label="Available Quantity" />
-                        <Input disabled={!isEditable} defaultValue={toy?.description} size="lg" type='text' name='description' label="Detail Description" />
+                        <Input defaultValue={toy?.price} size="lg" type='number' name='price' label="Price" />
+                        <Input defaultValue={toy?.quantity} size="lg" type='number' name='quantity' label="Available Quantity" />
+                        <Input defaultValue={toy?.description} size="lg" type='text' name='description' label="Detail Description" />
                     </div>
-                    {isEditable === false && <Button onClick={() => setIsEditable(true)} className="mt-6" fullWidth>
-                        Edit
-                    </Button>}
-                    {isEditable === true && <Button type='submit' className="mt-6" fullWidth>
+                    
+                    <Button type='submit' className="mt-6" fullWidth>
                         Update
-                    </Button>}
+                    </Button>
                 </form>
             </Card>
         </div>
